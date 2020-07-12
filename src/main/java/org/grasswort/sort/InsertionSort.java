05f896b2149ce5565f7dbd260ee7361806442202
许良良 <grasswort@qq.com>
2020-07-12 11:55:52 +0800

    冒泡排序 & 选择排序 & 插入排序

---
+++ src/main/java/org/grasswort/sort/InsertionSort.java
@@ -0,0 +1,28 @@
+package org.grasswort.sort;
+
+import org.grasswort.sort.util.IntArrayUtil;
+
+/**
+ * @author xuliangliang
+ * @Description 插入排序 O(N ^ 2)
+ * @Date 2020/7/12
+ */
+public class InsertionSort implements IMathArraySort {
+    /**
+     * 数组排序, 等同于 Arrays.sort(int[] array)
+     *
+     * @param array
+     */
+    @Override
+    public void sort(int[] array) {
+        // 0, 1 先保证小区间有序性
+        // 0, 2 再逐步扩大区间
+        // 0, ... n-1
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0 && array[j] < array[j - 1] ; j--) {
+                IntArrayUtil.swap(array, j, j - 1);
+            }
+        }
+
+    }
+}
